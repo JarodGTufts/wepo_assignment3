b7ff7ee40c14f73c050de4167985eac612420e0d
JarodGTufts <jarod.gowgiel@tufts.edu>
2018-02-20 17:53:10 +0000

    Formatting changes to enable custom css overload by site.less

--- template/src/pages/chat.js
+++ template/src/pages/chat.js
@@ -10,31 +10,33 @@ class Chat extends React.Component {
 
     render() {
         return (
-            <div className="row">
-                <div className="col-3">
-                    <div className="row">
-                        <Userlist
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-3">
+                        <div className="row">
+                            <Userlist
+                                service = {this.props.service}
+                            />
+                        </div>
+                        <div className="row">
+                        //Roomlist
+                        </div>
+                    </div>
+                    <div className="col-6">
+                        <Messenger
                             service = {this.props.service}
+                            activeRoom = {this.props.activeRoom}
+                            onJoinRoom = {this.props.onJoinRoom}
+                            username = {this.props.username}
                         />
                     </div>
-                    <div className="row">
-                    //Roomlist
-                    </div>
-                </div>
-                <div className="col-6">
-                    <Messenger
-                        service = {this.props.service}
-                        activeRoom = {this.props.activeRoom}
-                        onJoinRoom = {this.props.onJoinRoom}
-                        username = {this.props.username}
-                    />
-                </div>
-                <div className="col-3">
-                    <div className="row">
-                    //UserlistInRoom
-                    </div>
-                    <div className="row">
-                    //BannedUserlistInRoom
+                    <div className="col-3">
+                        <div className="row">
+                        //UserlistInRoom
+                        </div>
+                        <div className="row">
+                        //BannedUserlistInRoom
+                        </div>
                     </div>
                 </div>
             </div>
