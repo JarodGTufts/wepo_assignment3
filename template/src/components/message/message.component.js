e8d6dd6edd8dac3c7e9170af1e9264e5ab72004c
JarodGTufts <jarod.gowgiel@tufts.edu>
2018-02-20 13:45:16 +0000

    Spacing corrections for message.component.js to shut this linter up

--- template/src/components/message/message.component.js
+++ template/src/components/message/message.component.js
@@ -2,30 +2,28 @@ import React from 'react';
 
 function Message(props) {
 // Initialize local variables for props
-  let username = props.username;
-  let author = props.author;
-  let message = props.message;
-  // If the message was sent by the current user,
+    let username = props.username;
+    let author = props.author;
+    let message = props.message;
+    // If the message was sent by the current user,
     // return a message with the corerct class
-  if (author === username) {
-    return (
-      <div className='Message'>
-        <li className='list-group-item list-group-item-secondary bg-info my-message'>
-          {message} <sub> {author} </sub>
-        </li>
-      </div>
-    );
-  } 
-  // If the message was sent by another user, return a message
-  else {
-    return (
-      <div className='Message'>
-        <li className='list-group-item list-group-item-secondary other-message'>
-          {message} <sub> {author} </sub>
-        </li>
-      </div>
-    );
-  }
+    if (author === username) {
+        return (
+            <div className='Message'>
+                <li className='list-group-item list-group-item-secondary bg-info my-message'>
+                    {message} <sub> {author} </sub>
+                </li>
+            </div>
+        );
+    } else {
+        return (
+            <div className='Message'>
+                <li className='list-group-item list-group-item-secondary other-message'>
+                    {message} <sub> {author} </sub>
+                </li>
+            </div>
+        );
+    }
 }
 
 export default Message;
