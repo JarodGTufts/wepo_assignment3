a34bb518d3d44288aeb32cf38d279595dcfc9d1f
Andre <andre.rivera.arboleda@gmail.com>
2018-02-20 13:45:30 +0000

    Another merge

--- template/src/components/message-list/messeng-list.component.js
+++ template/src/components/message-list/messeng-list.component.js
@@ -12,21 +12,18 @@ class MessageList extends React.Component {
 
     render() {
         let messages = [];
-
         // Fill the messages array with the messages passed as props
         this.props.messages.forEach((msg, index) => {
-
             // Create new Message components for each member of the array
             messages.push(
                 <Message
                     message={msg.message}
                     author={msg.nick}
-                    sender = {this.props.sender}
-                    key = {'message' + index}
+                    username={this.props.username}
+                    key={'message' + index}
                 />)
 
         });
-
         // Return a new list of all of the messages
         return (
             <div className='messenger' >
