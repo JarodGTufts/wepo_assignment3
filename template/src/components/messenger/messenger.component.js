597f2dd04829be3fb5e42a8d1a580053bcb80daf
Andre <andre.rivera.arboleda@gmail.com>
2018-02-20 23:35:13 +0000

    One console log gone

--- template/src/components/messenger/messenger.component.js
+++ template/src/components/messenger/messenger.component.js
@@ -14,7 +14,6 @@ class Messenger extends React.Component {
     }
 
     render() {
-        console.log(this.props.messages);
         if(this.props.mode === 'roomchat') {
             return (
                 <div className='messenger'>
