dc03bac4950a836d822d36021d7abd6d49724a6e
Andre <andre.rivera.arboleda@gmail.com>
2018-02-20 13:40:23 +0000

    I hope this fixes it

--- template/src/components/message/message.component.js
+++ template/src/components/message/message.component.js
@@ -7,7 +7,6 @@ function Message(props) {
   let message = props.message;
   // If the message was sent by the current user,
     // return a message with the corerct class
-<<<<<<< HEAD
   if (author === username) {
     return (
       <div className='Message'>
@@ -27,25 +26,6 @@ function Message(props) {
       </div>
     );
   }
-=======
-    if (sender === myName) {
-        return (
-            <div className='Message'>
-                <li className='list-group-item list-group-item-secondary bg-info my-message'>
-                    {message} <sub> {sender} </sub>
-                </li>
-            </div>
-        );
-    } else {
-        return (
-            <div className='Message'>
-                <li className='list-group-item list-group-item-secondary bg-success other-message'>
-                    {message} <sub> {sender} </sub>
-                </li>
-            </div>
-        );
-    }
->>>>>>> 502026e1e618b0ae3f5ba2ed73678726dae4b83f
 }
 
 export default Message;
